5914c93c777e897f5b847ec032077627f3ca1f90
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-06-21 20:43:08 -0500

    Creado coleccion de aspirante, direccion y documentos

---
+++ src/api/aspirant/services/aspirant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aspirant service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::aspirant.aspirant');
