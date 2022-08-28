e86837d354915239942b1ba1cfdb026d36ba801a
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-08-28 11:52:07 -0500

    Instalado Cloudinary para manejar los media library e implementado

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,25 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
       playgroundAlways: false,
       apolloServer: {
         tracing: true,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
